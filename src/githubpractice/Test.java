0a5fadf273c9023db21bb4cc4e20bddeaf9579b0
kishorkumar118 <kishorekumar1828@gmail.com>
2024-01-30 15:18:11 +0530

    initial commit

---
+++ src/githubpractice/Test.java
@@ -0,0 +1,17 @@
+package githubpractice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =5;
+		int b = 3;
+		int sum = a+b;
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
