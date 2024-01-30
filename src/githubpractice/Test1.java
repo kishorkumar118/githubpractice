453796dda2b60894d1b94982f24fcb417cdaed37
kishorkumar118 <kishorekumar1828@gmail.com>
2024-01-30 15:51:08 +0530

    add new class to repo

---
+++ src/githubpractice/Test1.java
@@ -0,0 +1,16 @@
+package githubpractice;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =5;
+		int b = 3;
+		int sub = a-b;
+		
+		System.out.println(sub);
+		
+	}
+
+}
